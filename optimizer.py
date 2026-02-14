fe6b6767d042a361e4f0789b821076483bb13714
Cerulean Pebble <vismaydarshi@gmail.com>
2026-02-14 15:30:49 +0530

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,9 +1,1 @@
-def optimize_code(code):
-    optimized = code.replace(
-        "for i in range(len(x)):",
-        "for i, val in enumerate(x):"
-    )
 
-    energy_saved = "18%"
-
-    return optimized, energy_saved
